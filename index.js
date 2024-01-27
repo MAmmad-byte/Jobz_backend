2b06e9466fa7f694f5dcac24f13cb8986f58be5a
Muhammad Amamd <muhammadammad146@gmail.com>
2024-01-27 21:35:47 +0500

    troubleshooting

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 require("./db");
-// const userRoute = require("./routes/users")
-// const authRoute = require("./routes/auth")
+const userRoute = require("./routes/users")
+const authRoute = require("./routes/auth")
 const jobRoute = require("./routes/jobs")
 // const jobApplication = require("./routes/jobApplication")
 // const jobApplied = require("./routes/jobsApplied")
@@ -16,8 +16,8 @@ app.use(cors())
 app.use(helmet())
 app.use(compression())
 app.use(express.json())
-// app.use("/api/users", userRoute);
-// app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 app.use("/api/jobs", jobRoute);
 // app.use("/api/users/jobs", jobApplication);
 // app.use("/api/admin/users/appliedjobs", jobApplied);
