bd6592783adc432a01cc0841cdb2cf14af422588
Muhammad Amamd <muhammadammad146@gmail.com>
2024-01-27 21:40:01 +0500

    troubleshooting

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require("./db");
 const userRoute = require("./routes/users")
 const authRoute = require("./routes/auth")
 const jobRoute = require("./routes/jobs")
-// const jobApplication = require("./routes/jobApplication")
+const jobApplication = require("./routes/jobApplication")
 const jobApplied = require("./routes/jobsApplied")
 const cors = require('cors');
 const helmet = require('helmet')
@@ -19,7 +19,7 @@ app.use(express.json())
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/jobs", jobRoute);
-// app.use("/api/users/jobs", jobApplication);
+app.use("/api/users/jobs", jobApplication);
 app.use("/api/admin/users/appliedjobs", jobApplied);
 
 let port = process.env.PORT || 3000
