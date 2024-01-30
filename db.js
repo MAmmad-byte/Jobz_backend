6f54345e9bdc6d9c7169b7a88382d35c3281e264
Muhammad Amamd <muhammadammad146@gmail.com>
2024-01-30 20:49:51 +0500

    Ready to deploy

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const  mongoose  = require("mongoose");
 
 
-module.exports = mongoose.connect("mongodb+srv://masteram070:merapig@cluster0.qk8gi0i.mongodb.net/")
+module.exports = mongoose.connect("mongodb+srv://ammad:123412341234@cluster0.wwzvysv.mongodb.net/jobz")
 .then(()=>console.log("Connected to Database..."))
 .catch((err)=>console.log("Database Error:", err))
